5e3c61721c2b95ae331d3ae2619f8311ec471d7c
lgolen <36285034+lgolen@users.noreply.github.com>
2024-09-22 19:09:14 -0500

    Spring 2 - Story 2A Comments

--- Models/MessageRecord.cs
+++ Models/MessageRecord.cs
@@ -4,6 +4,8 @@
     {
         public MessageRecord(int roomID, string authorName, string text)
         {
+            // Changes for Sprint 2 -- User Story 2A -- Lonnie Golen
+            // Changes Sprint 2 -- As a message poster, I want to send markdown text that will be correctly formatted. --Lonnie Golen
             RoomID = roomID;
             Text = text;
             AuthorName = authorName;
@@ -17,6 +19,8 @@
 
         public string Text
         {
+            // Changes for Sprint 2 -- User Story 2A -- Lonnie Golen
+            // Changes Sprint 2 -- As a message poster, I want to send markdown text that will be correctly formatted. --Lonnie Golen
             get;
             private set;
         }
